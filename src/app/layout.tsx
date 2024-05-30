c85b372efd1e851afaa41095f958ff911de224ce
navindu-sachintha <navindu.social@gmail.com>
2024-05-30 15:12:13 +0530

    Added nextjs files

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Navindu Sachintha",
+  description: "Welcome to my Portfolio",
+  openGraph:{
+    title: "Navindu Sachintha",
+    description: "Welcome to my personal Portfolio",
+    siteName: "Navindu Sachintha",
+    images:[
+      {
+        url: "https://i.ibb.co/RC88yHP/Navindu.png",
+        width: 800,
+        height: 600,
+        alt: "Navindu Sachintha"
+      },
+    ]
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
