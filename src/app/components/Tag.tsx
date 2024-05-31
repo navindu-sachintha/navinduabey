ed720cffe5aee349912ac9f3a799efede0bb2725
navindu-sachintha <navindu.social@gmail.com>
2024-06-01 01:39:20 +0530

    Enhancement: Updated type conflict in tag

--- src/app/components/Tag.tsx
+++ src/app/components/Tag.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-const Tag = ({src,name,link}) => {
+const Tag = ({src,name,link}:{
+  src:string,
+  name:string,
+  link:string
+
+}) => {
   return (
     <a className='inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100' href={link} target="_blank" rel="noopener noreferrer">
         <img className='!mr-1' src={src} alt="" width="14" height="14"/>
