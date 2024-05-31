8f315f205dbe97d70aca7bedb5f7f85c4992cca2
navindu-sachintha <navindu.social@gmail.com>
2024-06-01 01:43:12 +0530

    Enhancement: Optimized with next image

--- src/app/components/Tag.tsx
+++ src/app/components/Tag.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Tag = ({src,name,link}:{
   src:string,
@@ -8,7 +9,12 @@ const Tag = ({src,name,link}:{
 }) => {
   return (
     <a className='inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100' href={link} target="_blank" rel="noopener noreferrer">
-        <img className='!mr-1' src={src} alt="" width="14" height="14"/>
+        <Image
+          src={src}
+          alt='tag'
+          width={14}
+          height={14} 
+        />
         {name}
     </a>
   )
